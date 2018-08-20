96a569f9bf83e2a331e02d75d8bd3a31991b8a46
Joseph D <jdicdican@nerubia.com>
2018-08-20 16:26:38 +0800

    Setup knowlaravel repo

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticlesController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::all();
+
+        return $articles;
+    }
+}
