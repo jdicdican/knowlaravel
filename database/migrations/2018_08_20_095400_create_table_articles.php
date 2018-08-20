55da8ae14a53f68e3c34db8f63ae68d4a6ad04d8
Marvin T <mtordillos@nerubia.com>
2018-08-20 19:03:02 +0800

    Feature:Articles Module (table & model)

---
+++ database/migrations/2018_08_20_095400_create_table_articles.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableArticles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->text('body');
+            $table->integer('author_id');
+            $table->timestamps();
+            $table->softdeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
