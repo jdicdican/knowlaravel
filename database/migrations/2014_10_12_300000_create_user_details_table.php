99d229fcbd1f58c84c08ab6ac5d8fee11734a375
John Gerald Agbayani <john.agbayani@idearobin.com>
2018-08-22 11:20:45 +0800

    Feature: New model UserDetail and added columns to users

--- database/migrations/2014_10_12_300000_create_user_details_table.php
+++ database/migrations/2014_10_12_300000_create_user_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_details', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email', 255)->nullable();
-            $table->string('password', 255);
-            $table->string('username', 255);
-            $table->tinyInteger('user_type');
-            $table->rememberToken();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_details');
     }
 }
