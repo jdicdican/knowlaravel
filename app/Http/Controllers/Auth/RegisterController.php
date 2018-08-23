8ffbb8d8e2042a2233b255196379d9b8cc6f4dee
John Gerald Agbayani <john.agbayani@idearobin.com>
2018-08-23 13:12:50 +0800

    Authentication module now using Laravel Authentication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboardjhghjgj';
 
     /**
      * Create a new controller instance.
